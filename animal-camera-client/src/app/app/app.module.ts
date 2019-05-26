6467ff30d17b6cb85941e13a77450acd56863c10
IlyaSolovyov <solovyov909@gmail.com>
2019-05-26 12:14:10 +0300

    Reorganized App Module folder. Updated .gitignore.

--- animal-camera-client/src/app/app/app.module.ts
+++ animal-camera-client/src/app/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+
+
 
 @NgModule({
   declarations: [
